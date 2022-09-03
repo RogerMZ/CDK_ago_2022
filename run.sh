2fa34010ee0e2514e05b11320ef17e37e748bb0a
RogerMZ <rogermz.projects@gmail.com>
2022-09-03 13:45:13 -0500

    Funcionalidad

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ docker stop api-rest || true && docker rm api-rest || true
 # Run container application
 docker run -itd --name api-rest -p 3000:3000 rogermz/miappcdk-ago22:1.0
 
+docker logs api-rest
+
